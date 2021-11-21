51b3de687e4aefcebf6d87bbee8cda151a653af1
lexingsen <lexingsen@163.com>
2021-11-21 08:10:37 +0800

    modify 16

--- 16-最大公约数和最小公倍数.c
+++ 16-最大公约数和最小公倍数.c
@@ -6,14 +6,24 @@ int gcd(int a, int b) {
     return b ? gcd(b, a % b) : a;
 }
 
+int gcd2(int a, int b) {
+    while (b) {
+        int c = a % b;
+        a = b;
+        b = c;
+        printf("a = %d, b = %d, c = %d\n", a, b, c);
+    }
+    return a;
+}
+
 int lcm(int a, int b) {
-    return a * b / gcd(a, b);
+    return a * b / gcd2(a, b);
 }
 
 int main() {
     int a = 0, b = 0;
     scanf("%d%d", &a, &b);
-    printf("gcd = %d\n", gcd(a, b));
-    printf("gcd = %d\n", lcm(a, b));
+    printf("gcd = %d\n", gcd2(a, b));
+    printf("lcm = %d\n", lcm(a, b));
     return 0;
 }
