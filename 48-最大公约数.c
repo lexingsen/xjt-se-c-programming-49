03b994089a34e18177e144c17e517d4a3c8fb789
lexingsen <lexingsen@163.com>
2021-12-04 11:17:21 +0800

    modify 48

--- 48-最大公约数.c
+++ 48-最大公约数.c
@@ -3,3 +3,11 @@
 int gcd(int a, int b) {
     return b ? gcd(b, a % b) : a;
 }
+
+int main() {
+    int n = 0;
+    int m = 0;
+    scanf("%d%d", &n, &m);
+    printf("gcd(%d, %d) = %d\n", n, m, gcd(n, m));
+    return 0;
+}
